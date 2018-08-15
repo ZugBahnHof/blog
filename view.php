e8c2f26893b56ae832c5b186b047fae35ee78263
Julian <leuckerj@gmail.com>
2018-08-15 22:17:39 +0200

    Basisfunktionen der View-Seite generieren

---
+++ view.php
@@ -0,0 +1,13 @@
+<?php
+$site_title = "Blog";
+$a2="active";
+$r2="&raquo;";
+$site_color = "orange";
+$site_color_text = "orange-text";
+$site_color_html = "#ff9800";
+include 'inc/header.inc.php';
+?>
+<h1>Blog</h1>
+<?php
+include 'inc/footer.inc.php';
+ ?>
