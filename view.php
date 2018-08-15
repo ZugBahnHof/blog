e9886b5a04ee20904bb50a8909de1eff1d12cf24
Julian <leuckerj@gmail.com>
2018-08-15 22:46:21 +0200

    Gefärbte Überschrift

--- view.php
+++ view.php
@@ -7,7 +7,7 @@ $site_color_text = "orange-text";
 $site_color_html = "#ff9800";
 include 'inc/header.inc.php';
 ?>
-<h1>Blog</h1>
+<h1 class="<?php echo $site_color_text; ?>">Blog: <i>*Hier Titel einfügen*</i> </h1>
 <?php
 include 'inc/footer.inc.php';
  ?>
